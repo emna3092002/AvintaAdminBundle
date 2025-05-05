a8964f47de7956c4e639a73c43a03f828ae20612
emna3092002 <emna.guefrech@esprit.tn>
2025-05-05 10:48:08 +0200

    Update DashboardMenuItem.php

--- src/Config/Menu/DashboardMenuItem.php
+++ src/Config/Menu/DashboardMenuItem.php
@@ -6,11 +6,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Contracts\Menu\MenuItemInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\MenuItemDto;
 use Symfony\Contracts\Translation\TranslatableInterface;
 
-/**
- * @see EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem::linkToDashboard()
- *
- * @author Javier Eguiluz <javier.eguiluz@gmail.com>
- */
+
 final class DashboardMenuItem implements MenuItemInterface
 {
     use MenuItemTrait;
