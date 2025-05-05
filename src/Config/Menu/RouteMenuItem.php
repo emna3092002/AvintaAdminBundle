9ae21384203e5bbb985b5f3f71f5ca6dbad23973
emna3092002 <emna.guefrech@esprit.tn>
2025-05-05 10:49:32 +0200

    Update RouteMenuItem.php

--- src/Config/Menu/RouteMenuItem.php
+++ src/Config/Menu/RouteMenuItem.php
@@ -6,11 +6,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Contracts\Menu\MenuItemInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\MenuItemDto;
 use Symfony\Contracts\Translation\TranslatableInterface;
 
-/**
- * @see EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem::linkToRoute()
- *
- * @author Javier Eguiluz <javier.eguiluz@gmail.com>
- */
+
 final class RouteMenuItem implements MenuItemInterface
 {
     use MenuItemTrait;
